44925c96a3e1d663874dc99c64bfe07d271e329c
BiancaLouS <biancalourencosilva@gmail.com>
2022-11-21 15:54:23 -0300

    Subindo todos os arquivos de desenvolvimento da atividade

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Construtora Dois Irmãos</title>
+
+    <link rel="styleesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <?php include('header.html'); ?> 
+    <section id="destaques" class="conteudo85">
+        <h1 class="titulo">Materiais</h1>
+        <article class="linha_subitens">
+            <div class="item_destaque">
+                <figure>
+                    <img src="" alt="">
+                </figure>
+            </div>
+        </article>
+    </section>
+</body>
+</html>
